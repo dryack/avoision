3860b74edd9bbe3a7db62e633a8eac02c84e436b
Dave Ryack <git.lamashtu@gmail.com>
2017-09-18 22:57:20 -0700

    Create redirector.js

---
+++ redirector.js
@@ -0,0 +1,24 @@
+// Catch requests before the browser sends them
+chrome.webRequest.onBeforeRequest.addListener(
+    function(details) {
+        var url = details.url;
+
+        return archiveUrlConstructor(url);
+    },
+    {
+        urls: [ "*://*.washingtonpost.com/*",
+                "*://wapo.com/*",
+                "*://arstechnica.com/*",
+                "*://buzzfeed.com/*",
+                "*://cnn.com/*"
+        ]
+    },
+    ['blocking']
+);
+
+// Build the archive.is request url
+function archiveUrlConstructor(url){
+    var archiver = 'https://archive.is/?run=1&url=';
+
+    return { redirectUrl: archiver + url };
+}
