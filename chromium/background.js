055bda6783e9346c8ee7a1e10a64f9058a8020f1
dryack <dave.ryack@gmail.com>
2018-01-07 20:50:05 -0800

    Major refactor of core code

--- chromium/background.js
+++ chromium/background.js
@@ -1,15 +1,23 @@
 var filter_list_state = 0;
 
 function toggleFilterlist() {
-    if (filter_list_state === 0) {
-        filter_list_state = 1;
-        chrome.browserAction.setBadgeText( {text: "OFF"});
-        return
+    switch(filter_list_state){
+        case 0:
+            filter_list_state = 1;
+            chrome.browserAction.setBadgeText( {text: "ARC"});
+            break;
+        case 1:
+            filter_list_state = 2;
+            chrome.browserAction.setBadgeText( {text: "STRP"});
+            break;
+        case 2:
+            filter_list_state = 3;
+            chrome.browserAction.setBadgeText( {text: "OFF"});
+            break;
+        case 3:
+            filter_list_state = 0;
+            chrome.browserAction.setBadgeText( {text: ""});
     }
-    if (filter_list_state === 1) {
-        filter_list_state = 0;
-        chrome.browserAction.setBadgeText( {text: ""});
-        return
-    }
+    return
 }
 chrome.browserAction.onClicked.addListener(toggleFilterlist);
--- chromium/redirector.js
+++ chromium/redirector.js
@@ -1,5 +1,5 @@
 //  avosion - Lightweight browser redirector for those who are addicted to the web of ideas
-//  Copyright (C) 2017 David Ryack
+//  Copyright (C) 2017 - 2018 David Ryack
 //
 //  This program is free software: you can redistribute it and/or modify
 //  it under the terms of the GNU Affero General Public License as published
@@ -15,9 +15,11 @@
 
 chrome.webRequest.onBeforeRequest.addListener(
     function(details) {
-        var url = details.url;
+        // if we're shut off return immediately
+        if(filter_list_state === 3) { return }
 
-        return strippedUrlConstructor(url);
+        var url = details.url;
+        return archiveUrlConstructor(url, filter_list_state);
     },
     {
         urls: ["<all_urls>"]
@@ -25,7 +27,153 @@ chrome.webRequest.onBeforeRequest.addListener(
     ['blocking']
 );
 
-function strippedUrlConstructor(url){
+// // Catch requests before the browser sends them
+// chrome.webRequest.onBeforeRequest.addListener(
+//     function(details) {
+//         var url = details.url;
+//
+//
+//         return archiveUrlConstructor(url);
+//     },
+//     // TODO load from a simple config file - hardcoding is terrible
+//     {
+//         urls: [ "*://*.washingtonpost.com/*",
+//                 "*://*.wapo.com/*",
+//                 "*://*.arstechnica.com/*",
+//                 "*://*.buzzfeed.com/*",
+//                 "*://*.nytimes.com/*",
+//                 "*://*.pjmedia.com/*",
+//                 "*://*.foxnews.com/*",
+//                 "*://*.heatst.com/*",
+//                 "*://*.dailykos.com/*",
+//                 "*://*.gamespot.com/*",
+//                 "*://*.espn.com/*",
+//                 "*://*.variety.com/*",
+//                 "*://*.abcnews/*",
+//                 "*://*.abc.net.au/*",
+//                 "*://*.ausgamers.com/*",
+//                 "*://*.breitbart.com/*",
+//                 "*://*.dailycaller.com/*",
+//                 "*://*.dailymail.co.uk/*",
+//                 "*://*.independent.co.uk/*",
+//                 "*://*.msnbc.com/*",
+//                 "*://*.nbcnews.com/*",
+//                 "*://*.newyorker.com/*",
+//                 "*://*.returnofkings.com/*",
+//                 "*://*.rollingstones.com/*",
+//                 "*://*.slate.com/*",
+//                 "*://*.telegraph.co.uk/*",
+//                 "*://*.yahoo.com/news/*",
+//                 "*://*.zerohedge.com/*",
+//                 "*://*.aftonbladet.se/*",
+//                 "*://*.avclub.com/*",
+//                 "*://*.birthmoviesdeath.com/*",
+//                 "*://*.boingboing.net/*",
+//                 "*://*.buzzfeed.com/*",
+//                 "*://*.cracked.com/*",
+//                 "*://*.dailydot.com/*",
+//                 "*://*.dailystormer.is/*",
+//                 "*://*.deadspin.com/*",
+//                 "*://*.destructoid.com/*",
+//                 "*://*.engadget.com/*",
+//                 "*://*.fusion.net/*",
+//                 "*://*.gamasutra.com/*",
+//                 "*://*.gameplanet.co.nz/*",
+//                 "*://*.gamesindustry.biz/*",
+//                 "*://*.gawker.com/*",
+//                 "*://*.gizmodo.com/*",
+//                 "*://*.huffingtonpost.com/*",
+//                 "*://*.io9.com/*",
+//                 "*://*.infowars.com/*",
+//                 "*://*.jalopnik.com/*",
+//                 "*://*.jezebel.com/*",
+//                 "*://*.kinja.com/*",
+//                 "*://*.kotaku.com/*",
+//                 "*://*.mediamatters.org/*",
+//                 "*://*.motherjones.com/*",
+//                 "*://*.neogaf.com/*",
+//                 "*://*.newmediarockstars.com/*",
+//                 "*://*.nymag.com/*",
+//                 "*://*.offworld.com/*",
+//                 "*://*.pcauthority.com.au/*",
+//                 "*://*.pcgamer.com/*",
+//                 "*://*.pointandclickbait.com/*",
+//                 "*://*.polygon.com/*",
+//                 "*://*.conservapedia.com/*",
+//                 "*://*.rationalwiki.org/*",
+//                 "*://*.rawstory.com/*",
+//                 "*://*.recode.net/*",
+//                 "*://*.rockpapershotgun.com/*",
+//                 "*://*.salon.com/*",
+//                 "*://*.splinternews.com/*",
+//                 "*://*.takimag.com/*",
+//                 "*://*.theblumpkin.com/*",
+//                 "*://*.thedailybeast.com/*",
+//                 "*://*.thegatewaypundit.com/*",
+//                 "*://*.littlegreenfootballs.com/*",
+//                 "*://*.theguardian.com/*",
+//                 "*://*.themarysue.com/*",
+//                 "*://*.theroot.com/*",
+//                 "*://*.theverge.com/*",
+//                 "*://*.vice.com/*",
+//                 "*://*.vox.com/*",
+//                 "*://*.wehuntedthemammoth.com/*",
+//                 "*://*.wired.com/*",
+//                 "*://*.houstonpress.com/*",
+//                 "*://*.sfist.com/*",
+//                 "*://*.laist.com/*",
+//                 "*://*.gothamist.com/*",
+//                 "*://*.worldstarhiphop.com/*",
+//                 "*://*.latimes.com/*",
+//                 "*://*.sfgate.com/*",
+//                 "*://*.sfchronicle.com/*",
+//                 "*://*.seattlepi.com/*",
+//                 "*://*.nypost.com/*",
+//                 "*://*.cnn.com/*",
+//                 "*://*.powerlineblog.com/*",
+//                 "*://*.mu.nu/*",
+//                 "*://*.bloomberg.com/*",
+//                 "*://*.washingtonexaminer.com/*",
+//                 "*://*.reallifemag.com/*",
+//                 "*://*.jeffro.wordpress.com/*",
+//                 "*://*.avoiceformen.com/*",
+//                 "*://*.hoover.org/*",
+//                 "*://*.sfexaminer.com/*",
+//                 "*://*.thecollegefix.com/*",
+//                 "*://*.campusreform.org/*",
+//                 "*://*.yiannopoulos.net/*",
+//                 "*://*.48hills.org/*",
+//                 "*://*.thefader.com/*",
+//                 "*://*.foxbusiness.com/*",
+//                 "*://*.pagesix.com/*",
+//                 "*://*.jammiewf.com/*",
+//                 "*://*.bigleaguepolitics.com/*",
+//                 "*://*.victorygirlsblog.com/*",
+//                 "*://*.althouse.blogspot.com/*",
+//                 "*://*.classicalvalues.com/*",
+//                 "*://*.businessinsider.com/*",
+//                 "*://*.eastbaytimes.com/*",
+//                 "*://*.eastbaytimes.com/*",
+//                 "*://*.xojane.com/*",
+//                 "*://*.cassiuslife.com/*",
+//                 "*://*.yournewswire.com/*",
+//                 "*://*.balkin.blogspot.com/*",
+//                 "*://*.newsbusters.org/*",
+//                 "*://*.therpgpundit.blogspot.com/*",
+//                 "*://*.townhall.com/*",
+//                 "*://*.spiked-online.com/*",
+//                 "*://*.cnbc.com/*",
+//                 "*://*.nationalreview.com/*",
+//                 "*://*.theamericanconservative.com/*",
+//                 "*://*.clickhole.com/*",
+//                 "*://*.amazon.com/*"
+//         ]
+//     },
+//     ['blocking'] // don't let the request go until we get back a redirectUrl (or other return in theory)
+// );
+
+// URL stripping
+function strip_url(url) {
     //adapted from https://github.com/jparise/chrome-utm-stripper because i'm lazy
     var utm_re = new RegExp('([\?\&]utm_(source|medium|term|campaign|content|cid|reader|name)=[^&#]+)', 'ig');
     var queryStringIndex = url.indexOf('?');
@@ -41,187 +189,114 @@ function strippedUrlConstructor(url){
     }
 }
 
-// Catch requests before the browser sends them
-chrome.webRequest.onBeforeRequest.addListener(
-    function(details) {
-        var url = details.url;
-        if(filter_list_state === 1) { return }
+// Amazon related
+function amznBuildDPUrl(amzurl, dp) {
+    return {redirectUrl: amzurl + dp};
+}
 
-        return archiveUrlConstructor(url);
-    },
-    // TODO load from a simple config file - hardcoding is terrible
-    {
-        urls: [ "*://*.washingtonpost.com/*",
-                "*://*.wapo.com/*",
-                "*://*.arstechnica.com/*",
-                "*://*.buzzfeed.com/*",
-                "*://*.nytimes.com/*",
-                "*://*.pjmedia.com/*",
-                "*://*.foxnews.com/*",
-                "*://*.heatst.com/*",
-                "*://*.dailykos.com/*",
-                "*://*.gamespot.com/*",
-                "*://*.espn.com/*",
-                "*://*.variety.com/*",
-                "*://*.abcnews/*",
-                "*://*.abc.net.au/*",
-                "*://*.ausgamers.com/*",
-                "*://*.breitbart.com/*",
-                "*://*.dailycaller.com/*",
-                "*://*.dailymail.co.uk/*",
-                "*://*.independent.co.uk/*",
-                "*://*.msnbc.com/*",
-                "*://*.nbcnews.com/*",
-                "*://*.newyorker.com/*",
-                "*://*.returnofkings.com/*",
-                "*://*.rollingstones.com/*",
-                "*://*.slate.com/*",
-                "*://*.telegraph.co.uk/*",
-                "*://*.yahoo.com/news/*",
-                "*://*.zerohedge.com/*",
-                "*://*.aftonbladet.se/*",
-                "*://*.avclub.com/*",
-                "*://*.birthmoviesdeath.com/*",
-                "*://*.boingboing.net/*",
-                "*://*.buzzfeed.com/*",
-                "*://*.cracked.com/*",
-                "*://*.dailydot.com/*",
-                "*://*.dailystormer.is/*",
-                "*://*.deadspin.com/*",
-                "*://*.destructoid.com/*",
-                "*://*.engadget.com/*",
-                "*://*.fusion.net/*",
-                "*://*.gamasutra.com/*",
-                "*://*.gameplanet.co.nz/*",
-                "*://*.gamesindustry.biz/*",
-                "*://*.gawker.com/*",
-                "*://*.gizmodo.com/*",
-                "*://*.huffingtonpost.com/*",
-                "*://*.io9.com/*",
-                "*://*.infowars.com/*",
-                "*://*.jalopnik.com/*",
-                "*://*.jezebel.com/*",
-                "*://*.kinja.com/*",
-                "*://*.kotaku.com/*",
-                "*://*.mediamatters.org/*",
-                "*://*.motherjones.com/*",
-                "*://*.neogaf.com/*",
-                "*://*.newmediarockstars.com/*",
-                "*://*.nymag.com/*",
-                "*://*.offworld.com/*",
-                "*://*.pcauthority.com.au/*",
-                "*://*.pcgamer.com/*",
-                "*://*.pointandclickbait.com/*",
-                "*://*.polygon.com/*",
-                "*://*.conservapedia.com/*",
-                "*://*.rationalwiki.org/*",
-                "*://*.rawstory.com/*",
-                "*://*.recode.net/*",
-                "*://*.rockpapershotgun.com/*",
-                "*://*.salon.com/*",
-                "*://*.splinternews.com/*",
-                "*://*.takimag.com/*",
-                "*://*.theblumpkin.com/*",
-                "*://*.thedailybeast.com/*",
-                "*://*.thegatewaypundit.com/*",
-                "*://*.littlegreenfootballs.com/*",
-                "*://*.theguardian.com/*",
-                "*://*.themarysue.com/*",
-                "*://*.theroot.com/*",
-                "*://*.theverge.com/*",
-                "*://*.vice.com/*",
-                "*://*.vox.com/*",
-                "*://*.wehuntedthemammoth.com/*",
-                "*://*.wired.com/*",
-                "*://*.houstonpress.com/*",
-                "*://*.sfist.com/*",
-                "*://*.laist.com/*",
-                "*://*.gothamist.com/*",
-                "*://*.worldstarhiphop.com/*",
-                "*://*.latimes.com/*",
-                "*://*.sfgate.com/*",
-                "*://*.sfchronicle.com/*",
-                "*://*.seattlepi.com/*",
-                "*://*.nypost.com/*",
-                "*://*.cnn.com/*",
-                "*://*.powerlineblog.com/*",
-                "*://*.mu.nu/*",
-                "*://*.bloomberg.com/*",
-                "*://*.washingtonexaminer.com/*",
-                "*://*.reallifemag.com/*",
-                "*://*.jeffro.wordpress.com/*",
-                "*://*.avoiceformen.com/*",
-                "*://*.hoover.org/*",
-                "*://*.sfexaminer.com/*",
-                "*://*.thecollegefix.com/*",
-                "*://*.campusreform.org/*",
-                "*://*.yiannopoulos.net/*",
-                "*://*.48hills.org/*",
-                "*://*.thefader.com/*",
-                "*://*.foxbusiness.com/*",
-                "*://*.pagesix.com/*",
-                "*://*.jammiewf.com/*",
-                "*://*.bigleaguepolitics.com/*",
-                "*://*.victorygirlsblog.com/*",
-                "*://*.althouse.blogspot.com/*",
-                "*://*.classicalvalues.com/*",
-                "*://*.businessinsider.com/*",
-                "*://*.eastbaytimes.com/*",
-                "*://*.eastbaytimes.com/*",
-                "*://*.xojane.com/*",
-                "*://*.cassiuslife.com/*",
-                "*://*.yournewswire.com/*",
-                "*://*.balkin.blogspot.com/*",
-                "*://*.newsbusters.org/*",
-                "*://*.therpgpundit.blogspot.com/*",
-                "*://*.townhall.com/*",
-                "*://*.spiked-online.com/*",
-                "*://*.cnbc.com/*",
-                "*://*.nationalreview.com/*",
-                "*://*.theamericanconservative.com/*",
-                "*://*.clickhole.com/*",
-                "*://*.amazon.com/*tag=*"
-        ]
-    },
-    ['blocking'] // don't let the request go until we get back a redirectUrl (or other return in theory)
-);
+function amznExtractASIN(url, typeASIN) {
+    var url_regex;
+    var finalLocation;
+    var dp;
+    if(typeASIN === "cruft") {
+        console.debug("Hit creativeASIN (cruft) decision (" + url + ")");
+        //old version, less universal: var finalLocation = url.substr(url.search(/&creativeASIN=\w{10}(&|$|\?)/)+14,10);
+        url_regex = new RegExp(/(.*amazon\.co(?:\..{2}|m)\/).*(?:creativeASIN=(\w{10})).*/);
+        finalLocation = url.match(url_regex);
+        dp = "dp/" + finalLocation[2];
 
-// Build the archive.is request url
-function archiveUrlConstructor(url){
-    // TODO do i want to leave this stuff hardcoded for ease of use, or do I want to make it configurable per website?
-    // possibly a combination where the user can configure websites they know of...
-    // meh, probably not really a possible in the way i thought
+    }
+    if(typeASIN === "embedded") {
+        console.debug("Hit creativeASIN (embedded) decision (" + url + ")");
+        //old version, less universal:  var finalLocation = url.substr(url.search(/\/ASIN\/w{10}($|.*)/) + 41, 10);
+        url_regex = new RegExp(/(.*amazon\.co(?:\..{2}|m)\/)(?:.+\/)(?:ASIN\/(\w{10})).*/);
+        finalLocation = url.match(url_regex);
+        dp = finalLocation[2];
+    }
+    var amzurl = finalLocation[1];
+    return amznBuildDPUrl(amzurl, dp);
+}
+
+function amznExtractDP(url) {
+    console.debug("Hit /dp/ decision (" + url + ")");
+    //old version, less universal:  var finalLocation = url.substr(url.search(/\/dp\/\w{10}($|.*)/) + 4, 10);
+    var finalLocation = url.match(/(.*amazon\.co(?:\..{2}|m)\/)(?:.+\/)?(dp\/\w{10}).*$/);
+    var dp = finalLocation[2];
+    var amzurl = finalLocation[1];
+    return amznBuildDPUrl(amzurl, dp);
+}
+
+function amznKillEtcReqs(url, typeofreq) {
+    console.debug("Killed amazon etc request: " + typeofreq);
+    console.debug("Intended URL: " + url);
+    return {redirectUrl: "https://127.0.0.1"};
+}
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// URL constructors
+function fullFeature(url) {
     var archiver = 'https://archive.is/?run=1&url=';
     // pjmedia crap
-    var pjmedia_singlepage = '?singlepage=true'; // avoid the irritating More button
+    var pjmediaSinglepage = '?singlepage=true'; // avoid the irritating More button
     var pjmediaRegex = new RegExp(/(pjmedia\.com)/); // detect we're on pjmedia site
     // amazon crap
-    var amazonReferralRegex = new RegExp(/(^.*amazon\.com.*(-19|-20|-21|-22|-23))/); // watching for referral links
-
+    var amazonURL = new RegExp(/(^.*amazon\.com.*)/);
     // decide how to build redirectUrl
-    if(url.match(amazonReferralRegex)){ // amazon de-referral
-
-        // if there's an ASIN available, let's just extract it and build the most basic possible URL without further
-        // processing
-        if(url.match(/&creativeASIN=\w{10}(&|$)/)) {
-            var finalLocation = url.substr(url.search(/&creativeASIN=\w{10}(&|$)/)+14,10);
-            return { redirectUrl: "https://amazon.com/dp/" + finalLocation };
+    if (url.match(amazonURL)) {
+        // these are assorted requests amazon performs during mouse-over and scrolling, i've been unable to learn much
+        // about them, but so far I haven't experienced any failures while blocking them
+        if (url.match(/amazon.com\/follow\/log-metrics/)) {
+            return amznKillEtcReqs(url, "follow/log-metrics");
         }
+        if (url.match(/amazon.com\/gp\/redirection\/mexico\.html/)) {
+            return amznKillEtcReqs(url, "redirection/mexico.html");
+        }
+        // where we can safely extract an ASIN
+        if (url.match(/&creativeASIN=\w{10}(&|$)/)) {
+            return amznExtractASIN(url, "cruft");
+        }
+        if (url.match(/^.*\/ASIN\/\w{10}\/.+/)) {
+            return amznExtractASIN(url, "embedded");
+        }
+        if (url.match(/^.*\/dp\/\w{10}.+/)) {
+            return amznExtractDP(url);
+        }
+
         // in other cases just strip out all the crap
-        var finalUrl = url.replace(/(tag=\w+(-19|-20|-21|-22|-23)(&|$))|(linkCode=\w+(&|$))|(creative=\w+(&|$))|(linkId=\w+(&|$))|(camp=\w+(&|$))/g,'');
-        console.debug(finalUrl);
+        var finalUrl = url.replace(/(tag=\w+(-19|-20|-21|-22|-23)(&|$))|(linkCode=\w+(&|$))|(creative=\w+(&|$))|(linkId=\w+(&|$))|(camp=\w+(&|$))/g, '');
         // TODO all the various amazon url references can pretty much be stripped as well - but there's alot of
         // experimenting to do in order to get that running
-        return { redirectUrl: finalUrl };
+        console.debug("finalUrl:" + finalUrl);
+        return {redirectUrl: finalUrl};
     }
 
-    if(url.match(pjmediaRegex)) { // avoid pjmedia More button bullshit
-        return { redirectUrl: archiver + url + pjmedia_singlepage };
+    if (url.match(pjmediaRegex)) { // avoid pjmedia More button bullshit
+        return {redirectUrl: archiver + url + pjmediaSinglepage};
     }
 
     // fallthrough option so to speak - our basic use
     return { redirectUrl: archiver + url };
 }
+function archiveOnly(url) {
+
+}
+function urlCleaningOnly(url) {
+
+}
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+function archiveUrlConstructor(url, selective_features) {
+    // TODO do i want to leave this stuff hardcoded for ease of use, or do I want to make it configurable per website?
+    // possibly a combination where the user can configure websites they know of...
+    // meh, probably not really a possible in the way i thought
+    switch (selective_features) {
+        case 0: return fullFeature(url);
+        case 1: return urlCleaningOnly(url);
+        case 2: return archiveOnly(url);
+        default:
+            return
+    }
+}
+
 // FEATURE Flag(?) to avoid amazon referrers
 
 // FEATURE allow whitelisting amazon referrers
